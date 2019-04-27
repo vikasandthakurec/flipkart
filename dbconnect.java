4d2e0ba822c37fca7aad9ba2a7526ca91f85827b
rana <krishnagotri@gmail.com>
2019-04-27 12:49:35 +0530

    update from dev

--- dbconnect.java
+++ dbconnect.java
@@ -1,6 +1,4 @@
 this is db connection file.
 hello, appending this line here.
-this is update from master branch.
-hello, i am updating this java code from development branch.
-again I am updating from dev branch
+this is an update from development branch.
 
